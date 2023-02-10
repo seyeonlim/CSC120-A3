ffdc6c59260a44890f60123b5ae7a2971a9ef964
Seyeon Lim <slim@smith.edu>
2023-02-09 20:20:50 -0500

    revert to original

--- Main.java
+++ Main.java
@@ -3,6 +3,5 @@ public class Main {
   public static void main(String[] args) {
     /* the next line will run your Conversation bot */
     Conversation.main(args);
-    System.out.print("HI");
   }
 }
