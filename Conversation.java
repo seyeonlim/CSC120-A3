a17cd8eca3622959f7d52e4c987ad598aa311786
Seyeon Lim <slim@smith.edu>
2023-02-13 18:14:01 -0500

    fixed my-your problem

--- Conversation.java
+++ Conversation.java
@@ -13,7 +13,8 @@ public class Conversation {
     };
 
     private static String getResponse(String input) {
-        String response = input.replace("i am", "You are");
+        String response = input;
+        response = response.replace("i am", "You are");
         response = response.replace("I am", "You are");
         response = response.replace("i'm", "You're");
         response = response.replace("I'm", "You're");
@@ -21,8 +22,11 @@ public class Conversation {
         response = response.replace("Im", "You're");
         response = response.replace("me", "you");
         response = response.replace("you", "I");
-        response = response.replace("my", "your");
+        response = response.replace("my", "TEMPORARY");
         response = response.replace("your", "my");
+        response = response.replace("TEMPORARY", "your");
+        
+        
 
         if (response.contains("You're") || response.contains("You are") || response.contains("I") || response.contains("your") || response.contains("my")) {
             response = response.substring(0, response.length() - 1) + "?";
