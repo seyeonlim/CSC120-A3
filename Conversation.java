c4fab82b9e6714636cae5a1dcb129f6059fd2022
Seyeon Lim <slim@smith.edu>
2023-02-13 18:37:30 -0500

    added comments

--- Conversation.java
+++ Conversation.java
@@ -1,7 +1,19 @@
+/*
+ * Course: CSC 120 (section 2)
+ * Author: Seyeon Lim
+ * Date: February 14, 2023
+ * Description: a simple chatbot code for assignment 3 that mirrors user's sentences by 
+ *              replacing pronouns and prints out a random canned phrase when there are 
+ *              no words to replace.
+ */
+
 import java.util.Scanner;
 import java.util.Random;
 
+
 public class Conversation {
+
+    // Making canned phrases
     private static String[] cannedPhrases = {
         "I see.",
         "That's interesting!",
@@ -12,6 +24,7 @@ public class Conversation {
         "OK."
     };
 
+    // Changes pronouns in user's input
     private static String getResponse(String input) {
         String response = input.replace("i am", "You are");
         response = response.replace("I am", "You are");
@@ -26,7 +39,7 @@ public class Conversation {
         response = response.replace("TEMPORARY", "your");
         
         
-
+        // Adding a question mark to response when the response isn't a canned phrase
         if (response.contains("You're") || response.contains("You are") || response.contains("I") || response.contains("your") || response.contains("my")) {
             response = response.substring(0, response.length() - 1) + "?";
         }
@@ -35,21 +48,29 @@ public class Conversation {
     }
 
     public static void main(String[] args) {
+
+        // Making scanner, random, and coversation, a variable for making the transcript
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
         String conversation = "";
 
+        // Asks user for number of rounds and scans the input
         System.out.print("Choose a number of rounds of conversation.\n");
         int rounds = scanner.nextInt();
         scanner.nextLine();
 
+        // Printing a greeting message and adding that to conversation
         System.out.println("Hi there! What's on your mind?");
         conversation += "Hi there! What's on your mind?\n";
 
+        // for statement to make the conversation run only for a given number of rounds
         for (int i = 0; i < rounds; i++) {
             String input = scanner.nextLine();
             conversation += input + "\n";
 
+            // Printing the response that ends with a question mark,
+            // which means that the response isn't a canned phrase.
+            // Use random index to print out a random canned phrase.
             String response = getResponse(input);
             if (response.endsWith("?")) {
                 System.out.println(response);
@@ -61,6 +82,7 @@ public class Conversation {
             }
         }
 
+        // Print transcript.
         System.out.println("Goodbye! Here is a transcript of our conversation:\n==========\nTRANSCRIPT\n==========\n" + conversation);
     }
 }
